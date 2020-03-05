e95d6b40d5998c804727a351f02ecc4f812adae1
Syresin Vitaliy from HomeNote <siresin@gmail.com>
2020-03-05 11:44:38 +0400

    csv. Add csv-example

---
+++ csv/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$csv_file = fopen("data(ansi).csv", "rt") or die("Error!");
+
+for ($i=0; $csv_data = fgetcsv($csv_file, 1000, ";"); i++)
+{
+	$num_row_csv_data = count($csv_data);
+
+	echo "<h3>Row number $i (Fields: $num_row_csv_data): </h3>";
+	for ($j=0; $j<$num_row_csv_data; $j++)  echo "[$j]: $csv_data[$j]<br>";
+}
+
+
+
+
+
+
+?>
