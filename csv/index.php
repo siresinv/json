996517ebbc34d92b6f6497689de065605a04e43b
Syresin Vitaliy from HomeNote <siresin@gmail.com>
2020-03-05 12:04:08 +0400

    csv. fix index.php. rename csv-file

--- csv/index.php
+++ csv/index.php
@@ -1,13 +1,13 @@
 <?php
 
-$csv_file = fopen("data(ansi).csv", "rt") or die("Error!");
+$csv_file = fopen("data_ansi.csv", "rt") or die("Error!");
 
-for ($i=0; $csv_data = fgetcsv($csv_file, 1000, ";"); i++)
+for ($i=0; $csv_data = fgetcsv($csv_file,1000,";"); i++)
 {
-	$num_row_csv_data = count($csv_data);
+	$num_col_on_row = count($csv_data);
 
-	echo "<h3>Row number $i (Fields: $num_row_csv_data): </h3>";
-	for ($j=0; $j<$num_row_csv_data; $j++)  echo "[$j]: $csv_data[$j]<br>";
+	echo "<h3>Row number $i (Fields: $num_col_on_row):</h3>";
+	for ($j=0; $j<$num_col_on_row; $j++)  echo "[$j]: $csv_data[$j]<br>";
 }
 
 
